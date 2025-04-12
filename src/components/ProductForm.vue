3d7c249ee2291ada9b79178d8909f94768b051a1
Augusto Araujo <macuto0301@gmail.com>
2025-04-12 04:19:04 -0400

    icono

--- src/components/ProductForm.vue
+++ src/components/ProductForm.vue
@@ -8,6 +8,7 @@
         <div class="flex-shrink-0">
           <svg
             class="h-5 w-5 text-red-400"
+            xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 20 20"
             fill="currentColor"
           >
